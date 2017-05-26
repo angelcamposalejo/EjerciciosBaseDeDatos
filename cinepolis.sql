93d8993a5e3640d1e829195a982dc9272b1fd0b8
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-26 18:37:22 -0500

    ejercicio crear nueva tabla y relacionarla con la primera

--- cinepolis.sql
+++ cinepolis.sql
@@ -9,3 +9,12 @@ alter table movie add clasificacion varchar(10) not null,
 add subtitulos varchar(20) not null,
 add lenguaje varchar(20) not null,
 add raiting varchar(10) not null;
+CREATE TABLE cine (
+    id_cine INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    ciudad VARCHAR(50) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    id_movie INT(10) NOT NULL,
+    FOREIGN KEY (id_movie)
+        REFERENCES movie (id_movie)
+);
