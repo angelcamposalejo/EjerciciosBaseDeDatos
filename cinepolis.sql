a75533aa3eef91b4e0655ea84ddd3ba901c01a12
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-26 18:32:19 -0500

    ejercicio alter table

--- cinepolis.sql
+++ cinepolis.sql
@@ -5,3 +5,7 @@ CREATE TABLE movie (
     movie VARCHAR(50) NOT NULL,
     duracion INT(10) NOT NULL
 );
+alter table movie add clasificacion varchar(10) not null,
+add subtitulos varchar(20) not null,
+add lenguaje varchar(20) not null,
+add raiting varchar(10) not null;
